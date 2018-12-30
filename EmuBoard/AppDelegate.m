e3982b5afe9b4943b3fd9b75703240fb8e01023e
Yoji Suzuki <yoji_suzuki@dwango.co.jp>
2018-12-30 11:55:28 +0900

    initial commit

---
+++ EmuBoard/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  EmuBoard
+//
+//  Created by 鈴木　洋司　 on 2018/12/30.
+//  Copyright © 2018年 SUZUKIPLAN. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
